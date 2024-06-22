a901952af4c791c529c113f2ab0ac7ece250bf03
Arise <arisedevs@gmail.com>
2024-06-22 18:14:29 +0800

    "Update mainContent constants and About section component"

--- app/constants/mainContent.tsx
+++ app/constants/mainContent.tsx
@@ -1,21 +1,21 @@
-
 const mainContent = {
   name: "Jude Arais",
   title: "Web Developer",
   phrase:
     "I create dynamic and responsive websites using modern technologies. Let's bring your digital vision to life with intuitive design and seamless functionality.",
-  about: `I'm a website developer with a passion for building responsive
-  and dynamic websites through cutting-edge technologies. My journey into
-  the world of web development began with a curiosity to understand how
+  about: `I am transitioning into a career in Website Development from being a Workforce Specialist, where I honed my analytical skills and attention to detail. 
+  My journey into the world of web development began with a curiosity to understand how
   the digital landscape works, and since then, I've immersed myself in
-  the art and science of creating engaging online experiences.`,
+  the art and science of creating engaging online experiences. 
+  While I may be new to web development, I am eager to learn and dedicated to mater new technologies. 
+  My previous experience has equipped me with a strong work ethic and the ability to quickly adapt to new challenges, ensuring I can deliver high-quality results for your business.`,
 };
 
 export const sectionHeader = {
-  about: "About",
+  about: "About Me",
   technologies: "Technologies",
   experience: "Experience",
   projects: "Projects",
-}
+};
 
 export default mainContent;
--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -6,8 +6,8 @@ function About() {
   return (
     <section className="mb-20">
       <h1 id={sectionHeader.about.toLowerCase()} className="mb-6 section text-2xl text-secondary-light font-medium">{sectionHeader.about}</h1>
-      <div className="flex justify-center items-center flex-col xl:flex-row xl:gap-4 pb-4">
-        <Image src="images/about-img.svg" className="xl:order-last w-44 mb-5 xl:mb-0 xl:flex-2" alt="profile" priority width={200} height={200}/>
+      <div className="flex justify-center items-center flex-col pb-4">
+        <Image src="images/about-img.svg" className=" w-44 mb-5" alt="profile" priority width={200} height={200}/>
         <p className="leading-7 xl:flex-1">{mainContent.about}</p>
       </div>
     </section>
