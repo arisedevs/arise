92875d38ef1ecfd2ff2e9102357552a3d05d3494
Arise <arisedevs@gmail.com>
2024-06-23 11:40:14 +0800

    "Update mainContent constants: changed 'about' section text"

--- app/constants/mainContent.tsx
+++ app/constants/mainContent.tsx
@@ -3,8 +3,8 @@ const mainContent = {
   title: "Web Developer",
   phrase:
     "I create dynamic and responsive websites using modern technologies. Let's bring your digital vision to life with intuitive design and seamless functionality.",
-  about: `I am transitioning into a career in Website Development from being a Workforce Specialist, where I honed my analytical skills and attention to detail. 
-  My journey into the world of web development began with a curiosity to understand how
+  about: `I am interested in pursuing a career in Website Development, leveraging my current role as a Workforce Specialist, where I have honed my analytical skills and attention to detail. 
+  sMy journey into the world of web development began with a curiosity to understand how
   the digital landscape works, and since then, I've immersed myself in
   the art and science of creating engaging online experiences. 
   While I may be new to web development, I am eager to learn and dedicated to mater new technologies. 
