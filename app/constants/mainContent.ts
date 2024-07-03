e3a9461989910048eeeb727d1737e08cb1c0755f
Arise <arisedevs@gmail.com>
2024-07-03 21:44:28 +0800

    "Modify file: About.tsx, mainContent.ts"

---
+++ app/constants/mainContent.ts
@@ -0,0 +1,19 @@
+const mainContent = {
+  name: "Jude Arais",
+  title: "Frontend Developer",
+  phrase:
+    "I develop dynamic and responsive websites utilizing modern technologies. Let's bring your digital vision to life with intuitive design and seamless functionality.",
+  about: `I am a Workforce Specialist transitioning into Software Engineering, with a focus on Frontend Development. In my current role, I've developed strong analytical skills and a keen attention to detail, which I aim to leverage in my new career path.\n
+My journey into the world of web development began with a curiosity to understand how the digital landscape works. Since then, I've immersed myself in the art and science of creating engaging online experiences.
+Though I am new to this career path, I am committed to continuous learning and eager to adopt new technologies.\n
+My professional background has equipped me with strong work ethic and the ability to quickly adapt to new challenges, ensuring that I can deliver high-quality results.`,
+};
+
+export const sectionHeader = {
+  about: "About",
+  technologies: "Technologies",
+  experience: "Experience",
+  projects: "Projects",
+};
+
+export default mainContent;
--- app/sections/About.tsx
+++ app/sections/About.tsx
@@ -3,12 +3,16 @@ import Image from "next/image";
 
 function About() {
 
+  const aboutParagraphs = mainContent.about.split('\n\n');
+
   return (
     <section className="mb-20">
       <h1 id={sectionHeader.about.toLowerCase()} className="mb-6 section text-2xl text-secondary-light font-medium">About Me</h1>
       <div className="flex justify-center items-center flex-col pb-4">
         <Image src="images/about-img.svg" className=" w-44 mb-5" alt="profile" priority width={200} height={200}/>
-        <p className="leading-7 xl:flex-1">{mainContent.about}</p>
+        {aboutParagraphs.map((paragraph, index) => (
+            <p key={index} className="mb-4">{paragraph}</p>
+          ))}
       </div>
     </section>
   );
