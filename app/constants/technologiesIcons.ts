9e8d70dacdb4d895c9acdb2a28a48fcb442c54f4
Arise <arisedevs@gmail.com>
2024-08-09 13:17:57 +0800

    "Remov NextJS icon import and reference from stacksIconObj"

--- app/constants/technologiesIcons.ts
+++ app/constants/technologiesIcons.ts
@@ -1,11 +1,10 @@
 import { SiTailwindcss, SiReact, SiDaisyui, SiTypescript, SiNodedotjs, SiExpress, SiPostgresql, SiGithub, SiGit, SiVercel, SiMongodb} from "react-icons/si"
 import { PiBracketsCurly } from "react-icons/pi";
-import { RiNextjsFill } from "react-icons/ri";
+// import { RiNextjsFill } from "react-icons/ri";
 
 const stacksIconObj = {
     "React": SiReact,
     "Typescript": SiTypescript,
-    "NextJS": RiNextjsFill,
     "Tailwind CSS": SiTailwindcss,
     "DaisyUI": SiDaisyui,
     "Node.js": SiNodedotjs,
